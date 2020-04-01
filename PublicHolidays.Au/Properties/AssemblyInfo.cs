6cde9d7526599f4b2a24be43cb310fcabf1ab40e
Ryan Bartsch <rbartsch@adam.com.au>
2020-04-01 14:14:01 +1030

    converted projects to netcoreapp2.1

--- PublicHolidays.Au/Properties/AssemblyInfo.cs
+++ PublicHolidays.Au/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PublicHolidays.Au")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility for date calculations that takes Australian public holidays into consideration.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("yesmarket")]
 [assembly: AssemblyProduct("PublicHolidays.Au")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
